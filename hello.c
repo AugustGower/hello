320c6b9fe455deb6c149937261cdbca1f2dc0c4a
August <agower2@huskers.unl.edu>
2021-08-26 15:17:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: August Gower
+ *          agower2@huskers.unl.edu
+ * Date: 8/24/2021
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("August Gower\n");
+
+  return 0;
+}
